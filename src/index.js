758f65ed3ec7e80d0aa121a5166e89c80e509ebd
Michael Muchiri <muchirimichael2222@gmail.com>
2023-10-07 22:18:33 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // Your code here
+fetch('http://localhost:3000/characters')
+.then(res=>res.json())
+.then(data=>{
+    const div=document.querySelector('#character-bar')
+    data.forEach((animals)=>{
+        const span=document.createElement('span')
+        span.innerText=animals.name
+        div.appendChild(span)
+
+        span.addEventListener('click',()=>{
+            const p=document.querySelector('p')
+            const img=document.querySelector('img')
+            const span2=document.querySelector('#vote-count')
+
+            p.innerText=animals.name + animals.id
+            img.src=animals.image
+            span2.innerText=animals.votes
+        })
+        
+       
+
+    })
+    const form=document.querySelector('#votes-form')
+    const span2=document.querySelector('#vote-count')
+    form.addEventListener('submit',event=>{
+        event.preventDefault()
+        const input=document.querySelector('#votes')
+        console.log(input.value)
+        span2.innerText=input.value
+        form.reset()
+    })    
+})
